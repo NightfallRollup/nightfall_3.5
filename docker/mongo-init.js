5056f07095dd85b653bab19d74630bd400037a15
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2023-03-06 15:15:21 +0000

    fix: fix db use command on init script

--- docker/mongo-init.js
+++ docker/mongo-init.js
@@ -7,7 +7,7 @@ const TRANSACTIONS_COLLECTION = 'transactions ';
 const SUBMITTED_BLOCKS_COLLECTION = 'blocks';
 const COMMITMENTS_COLLECTION = 'commitments';
 
-use(DB_CLIENT_NAME);
+db.getSiblingDB(DB_CLIENT_NAME);
 db[TRANSACTIONS_COLLECTION].createIndex({ transactionHash: 1 });
 db[SUBMITTED_BLOCKS_COLLECTION].createIndex({ blockNumberL2: -1 });
 db[SUBMITTED_BLOCKS_COLLECTION].createIndex({ blockHash: -1 });
@@ -16,7 +16,7 @@ db[COMMITMENTS_COLLECTION].createIndex({ isNullifiedOnChain: 1 });
 db[COMMITMENTS_COLLECTION].createIndex({ isOnChain: 1 });
 db[COMMITMENTS_COLLECTION].createIndex({ nullifier: 1 });
 
-use(DB_OPTIMIST_NAME);
+db.getSiblingDB(DB_OPTIMIST_NAME);
 db[TRANSACTIONS_COLLECTION].createIndex({ transactionHash: 1 });
 db[TRANSACTIONS_COLLECTION].createIndex({ transactionHashL1: 1 });
 db[TRANSACTIONS_COLLECTION].createIndex({ commitments: 1 });
