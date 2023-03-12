97281f4ce2d08c6709d3c3de2ec7c175351da3ac
David <druiz0992@gmail.com>
2023-03-12 18:44:49 +0100

    fix: update

--- nightfall-optimist/docker-tx-submitted-entrypoint.sh
+++ nightfall-optimist/docker-tx-submitted-entrypoint.sh
@@ -6,7 +6,4 @@ then
   while ! nc -z ${BLOCKCHAIN_WS_HOST} ${BLOCKCHAIN_PORT}; do sleep 3; done
 fi
 
-# Launch Transaction Submitted Workers
-if [ "${OPTIMIST_TX_WORKER_COUNT}" ]; then
-  exec "$@"
-fi
+exec "$@"
